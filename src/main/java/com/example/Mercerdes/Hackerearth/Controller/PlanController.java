375ff96a5fd0c1b3e65f706bb560c34119cc048f
naman.ajmera <naman.ajmera@globallogic.com>
2021-08-29 12:26:39 +0530

    cross origin added for netlify

--- src/main/java/com/example/Mercerdes/Hackerearth/Controller/PlanController.java
+++ src/main/java/com/example/Mercerdes/Hackerearth/Controller/PlanController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin(origins = {"http://localhost:3000/","https://calendar-planner.netlify.app/"})
 @RequestMapping("/api")
 public class PlanController {
 
